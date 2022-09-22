ae096a95345e568ff7a06c7cdaa43745d8c3c59a
Arthur Rosch <rosch.arthur@gmail.com>
2022-09-22 17:57:11 -0300

    Controle de Empresa Pronto

--- script.js
+++ script.js
@@ -1,24 +1,63 @@
-class Funcionário {
+let productList = [],
+  manager = [],
+  seller = [];
+class Employee {
   Name;
   Wage;
 }
-class Manager extends Funcionário {
+class Manager extends Employee {
   Department;
 
-  ExibirInformation() {
-    console.log(`Informação: ${this.Name} ${this.Wage} ${this.Department}`);
-  }
-}
-class Seller extends Funcionário {
-  Commission_Percentage;
-
-  CalcularSalario() {}
-  ExibirInformation() {
+  View_Manager_Information() {
     console.log(
-      `Informação: ${this.Name} ${this.Wage} ${this.Commission_Percentage}`
+      `Informação Gerente \nNome:${this.Name} \nSalario:${this.Wage}R$ \nDepartamento:${this.Department}`
     );
   }
 }
+class Seller extends Employee {
+  Commission_Percentage;
+
+  Calculate_Salary() {}
+  View_Seller_Information() {
+    console.log(
+      `Informação Vendedor \nNome:${this.Name} \nSalario:${this.Wage}R$ \nComissão:${this.Commission_Percentage}%`
+    );
+  }
+}
+class Product {
+  Name;
+  Price;
+}
+class Sell {
+  Seller;
+  ProductList;
+  FinalValue;
+
+  Add_New_Product() {
+    let newProduct = new Product();
+    newProduct.Name = String(prompt("Nome do Produto:"));
+    newProduct.Price = Number(prompt("Valor do Produto:"));
+    productList.push(newProduct);
+    productList.forEach((check) => {
+      if (check.Name == "" || check.Price == "") {
+        productList.pop(newProduct);
+        alert("Não Foi Possível Cadastrar,Adicione valores Validos");
+      }
+    });
+    this.ProductList = productList;
+  }
+  Calculate_Total() {
+    var result = 0;
+    productList.forEach((value) => {
+      result = Number(result + value.Price);
+    });
+    return result;
+  }
+  Finalize_Sale() {
+    alert(`Vendedor: ${this.Seller} \nValor Final: ${this.FinalValue}`);
+  }
+}
+
 let loop = true;
 while (loop) {
   let choice = Number(
@@ -32,6 +71,7 @@ while (loop) {
       registerManager();
       break;
     case 3:
+      sales();
       break;
     case 4:
       loop = false;
@@ -41,15 +81,78 @@ while (loop) {
       break;
   }
 }
+
 function registerSeller() {
   let newSeller = new Seller();
   newSeller.Name = String(prompt("Nome do Vendedor:"));
-  newSeller.Wage = parseFloat(prompt("Salario do Vendedor:"));
+  newSeller.Wage = Number(prompt("Salario do Vendedor:"));
   newSeller.Commission_Percentage = parseFloat(prompt("Comissão do Vendedor:"));
+  seller.push(newSeller);
+  seller.forEach((check) => {
+    if (
+      check.Name == "" ||
+      check.Wage == "" ||
+      check.Commission_Percentage == ""
+    ) {
+      seller.pop(newSeller);
+      alert("Não Foi Possível Cadastrar,Adicione valores Validos");
+    } else {
+      newSeller.View_Seller_Information();
+    }
+  });
 }
 function registerManager() {
   let newManager = new Manager();
   newManager.Name = String(prompt("Nome do Gerente:"));
-  newManager.Wage = parseFloat(prompt("Salario do Gerente:"));
+  newManager.Wage = Number(prompt("Salario do Gerente:"));
   newManager.Department = String(prompt("Departamento do Gerente:"));
+  manager.push(newManager);
+  newManager.View_Manager_Information();
+  manager.forEach((check) => {
+    if (check.Name == "" || check.Wage == "" || check.Department == "") {
+      manager.pop(newManager);
+      alert("Não Foi Possível Cadastrar,Adicione valores Validos");
+    } else {
+      newManager.View_Manager_Information();
+    }
+  });
+}
+function sales() {
+  let loopSales = true;
+  if (seller.length === 0) {
+    alert("Cadastre um Vendedor Primeiro");
+  } else {
+    let newSell = new Sell();
+    newSell.Seller = String(prompt("Nome do Vendedor:"));
+    seller.forEach((seller) => {
+      if (seller.Name == newSell.Seller) {
+        while (loopSales) {
+          let choiceSales = Number(
+            prompt("1-Adicionar Produtos \n2-Finalizar Venda \n3-Sair")
+          );
+          switch (choiceSales) {
+            case 1:
+              newSell.Add_New_Product();
+              newSell.FinalValue = newSell.Calculate_Total();
+              break;
+            case 2:
+              if (productList.length == 0) {
+                alert("Adicione um Produto Primeiro");
+              } else {
+                newSell.Finalize_Sale();
+              }
+              break;
+            case 3:
+              loopSales = false;
+              break;
+            default:
+              alert("Opção Invalida!!!");
+              break;
+          }
+        }
+      } else {
+        alert("Vendedor Invalido!!!");
+      }
+    });
+  }
 }
